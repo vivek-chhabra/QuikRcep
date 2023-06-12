f4e760fccdfd48dfe1cc39e52ee5e25619f11890
vivek chhabra <vivekcb8642@gmail.com>
2023-06-12 23:47:58 +0530

    fix bugs

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import RecipesProvider, { RecipesContext } from "./context/RecipesContext";
 import NewRecipeProvider from "./context/NewRecipeContext";
 import DetailedRecipe from "./pages/recipe/DetailedRecipe";
 import CreateRecipe from "./pages/create/CreateRecipe";
+import { ThemeContext } from "./context/ThemeContext";
 import { useState, useContext } from "react";
 import NavBar from "./components/NavBar";
 import Home from "./pages/home/Home";
 import "./App.css";
-import { ThemeContext } from "./context/ThemeContext";
-
 
 function App() {
     const { recipes } = useContext(RecipesContext);
     const { ide } = useParams();
 
     // dark mode
-    const {isDarkMode} = useContext(ThemeContext);
+    const { isDarkMode } = useContext(ThemeContext);
 
     return (
-        <div className={isDarkMode ? "App dark" :"App"}>
+        <div className={isDarkMode ? "App dark" : "App"}>
             <NavBar />
             <Routes>
+                <Route path="/QuikRcep" element={<Navigate to="/" />} />
                 <Route path="/" element={<Home />} />
                 <Route path="/:ide" element={<DetailedRecipe recipes={recipes} />} />
                 <Route path="/create" element={<CreateRecipe />} />
