9a955044d7483579b05e062cbfbdc0162800e4df
Vivek chhabra <105328667+vivek-chhabra@users.noreply.github.com>
2023-06-13 17:46:44 +0530

    Update RecipeCard.js

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -18,6 +18,7 @@ export default function RecipeCard(props) {
 
     // to delete Recipe Card
     const handleDelete = async () => {
+        alert('vivek)
         await deleteRecipe(id);
         await window.location.reload();
         navigate('/QuikRcep')
